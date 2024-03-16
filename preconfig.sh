f4518d3641a9f68cd76942fca410c19823cfb0ae
Har3on <115350099+Hareon2@users.noreply.github.com>
2024-03-16 16:23:07 +0300

    Update preconfig.sh

--- preconfig.sh
+++ preconfig.sh
@@ -3,16 +3,14 @@
 # Устанавливаем пароль пользователя vagrant
 password="vagrant"
 
-# Путь к директории с ключами
-ssh_dir="/home/runner/.ssh"
-
 # Проверяем наличие приватного ключа
-if [ -f "$ssh_dir/id_rsa" ]; then
-    echo "everything is okay"
+if [ -f /home/runner/.ssh/id_rsa ]; then
+    echo "Everything is okay"
 else
-    # Если файл конфигурации отсутствует, создать его
+    # Если файл конфигурации отсутствует, создаем его
     # Создаем SSH ключ без ввода пароля (пустой пароль)
-    ssh-keygen -t rsa -b 4096 -N '' -f "$ssh_dir/id_rsa"
-    cat "$ssh_dir/id_rsa.pub" | sshpass -p "$password" ssh vagrant@192.168.0.111 'mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'
+    ssh-keygen -t rsa -b 4096 -N '' -f /home/runner/.ssh/id_rsa
+    cat /home/runner/.ssh/id_rsa.pub | sshpass -p "$password" ssh vagrant@192.168.0.111 'mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'
 fi
+
 
