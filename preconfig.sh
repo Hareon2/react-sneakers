17f214f535a34a9fa32dc41ae510630e7914c56c
Har3on <115350099+Hareon2@users.noreply.github.com>
2024-03-16 15:56:09 +0300

    Create preconfig.sh

---
+++ preconfig.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Устанавливаем пароль пользователя vagrant
+password="vagrant"
+
+# Проверяем наличие приватного ключа
+if [ -f ~/.ssh/id_rsa ]; then
+    ansible-playbook -i inventory/cluster all.yaml
+else
+    # Если файл конфигурации отсутствует, создать его
+    # Создаем SSH ключ без ввода пароля (пустой пароль)
+    ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa
+    cat ~/.ssh/id_rsa.pub | sshpass -p "$password" ssh vagrant@$VAGRANT_IP_ADDRESS 'mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'
+    ansible-playbook -i inventory/cluster all.yml
+fi
